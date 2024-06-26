f4355f1f00e2787ef2d4422eddcfa14b14d04497
Mykhailo Kravchenko <rezkiy37@gmail.com>
2024-06-26 20:46:46 +0200

    create bare calendar screen

---
+++ src/screens/CalendarScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function CalendarScreen() {
+  return (
+    <View>
+      <Text>Calendar screen</Text>
+    </View>
+  );
+}
+
+export default CalendarScreen;
