e7b769e6adda1d6dad5c5a7d187521703a096816
Mykhailo Kravchenko <rezkiy37@gmail.com>
2024-06-26 21:29:50 +0200

    integrate TimePicker

--- src/components/TimePicker/index.tsx
+++ src/components/TimePicker/index.tsx
@@ -1,12 +1,74 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {
+  forwardRef,
+  useCallback,
+  useImperativeHandle,
+  useRef,
+  type ForwardedRef,
+} from 'react';
+import BottomSheet, {
+  BottomSheetBackdrop,
+  BottomSheetView,
+  type BottomSheetBackdropProps,
+} from '@gorhom/bottom-sheet';
+import {StyleSheet, Text} from 'react-native';
 
-function TimePicker() {
+const snapPoints = ['50%', '75%'];
+
+type TimePickerRef = {
+  open: () => void;
+};
+
+function TimePicker(props: unknown, ref: ForwardedRef<TimePickerRef>) {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+
+  const renderBackdrop = useCallback(
+    (bottomSheetBackdropProps: BottomSheetBackdropProps) => (
+      <BottomSheetBackdrop
+        {...bottomSheetBackdropProps}
+        disappearsOnIndex={-1}
+        appearsOnIndex={0}
+      />
+    ),
+    [],
+  );
+
+  useImperativeHandle(ref, () => ({
+    open: () => {
+      bottomSheetRef.current?.snapToIndex(0);
+    },
+  }));
+
   return (
-    <View>
-      <Text>TimePicker</Text>
-    </View>
+    <BottomSheet
+      ref={bottomSheetRef}
+      index={-1}
+      snapPoints={snapPoints}
+      enablePanDownToClose
+      backdropComponent={renderBackdrop}
+      onChange={handleSheetChanges}>
+      <BottomSheetView style={styles.contentContainer}>
+        <Text>TimePicker</Text>
+      </BottomSheetView>
+    </BottomSheet>
   );
 }
 
-export default TimePicker;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: 'grey',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
+
+export type {TimePickerRef};
+
+export default forwardRef(TimePicker);
