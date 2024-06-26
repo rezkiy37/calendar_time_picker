a6bf9434b06fd6b3ddb4e777c424a79fcae935e9
Mykhailo Kravchenko <rezkiy37@gmail.com>
2024-06-26 20:59:10 +0200

    create bare TimePicker

---
+++ src/components/TimePicker/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function TimePicker() {
+  return (
+    <View>
+      <Text>TimePicker</Text>
+    </View>
+  );
+}
+
+export default TimePicker;
